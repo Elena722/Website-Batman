ec96ccb1382e0a349c9701e6e422c42645234e47
Elena Santasheva <57711502+Elena722@users.noreply.github.com>
2020-03-27 11:23:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,24 +12,5 @@ $(document).ready(function () {
     $(this).toggleClass('tabs-item-active');
 
   });
-  $(".play").click(function() {
-    $.fancybox({
 
-    padding: 0,
-        'autoScale'     : false,
-        'transitionIn'  : 'none',
-        'transitionOut' : 'none',
-        'title'         : this.title,
-        'width'         : 795,
-        'height'        : 447,
-        'href'          : this.href.replace(new RegExp("watch.*v=","i"), "v/"),
-        'type'          : 'swf',
-        'swf'           : {
-        'wmode'             : 'transparent',
-        'allowfullscreen'   : 'true'
-         }
-
-    });
-    return false;
-});
 });
